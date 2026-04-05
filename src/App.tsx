a326ebb4c93824d809eda910e3930b7aebcaaac7
Piotr Falkowski <pfal711@gmail.com>
2026-04-06 00:34:18 +0200

    fix: added basename for router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/za-ile-litr">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
